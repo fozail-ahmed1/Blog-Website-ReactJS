b8079faa7910d6383597336586a83a84acc64930
fozail-ahmed1 <fozailahmed02@gmail.com>
2023-04-05 02:06:47 +0530

    please accept my 'commit'-ment!Thats what she said

---
+++ src/UserCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UserCard = (props) => {
+    return(
+        <div className="ui card">
+            <div className="content">
+                <div className="header">
+                  {props.headername}
+                </div>
+                <div className="description">
+                    {props.children}
+                </div>
+                <div className="ui bottom button">
+                    <i className="add icon"></i>
+                    Add Friend
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom";
+import profile1 from './images/micheal.jpg';
+import profile2 from './images/dwight.jpg';
+import profile3 from './images/jimAndPam.jpg';
+import profile4 from './images/andy.PNG';
+import SingleComment from "./SingleComment";
+import UserCard from "./UserCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return(
+        <div className="ui comments">
+            <UserCard
+            headername='Micheal Gary Scott'>
+                <SingleComment 
+                name='Micheal'
+                time='Today at 5:00PM' 
+                text='Thats what she said'
+                picture={profile1}
+                />
+            </UserCard>
+            <UserCard
+            headername='Dwight Schurte'>
+            <SingleComment 
+            name='Dwight'
+            time='Today at 6:00PM'
+            text='Do you want to form an alliance with me?'
+            picture={profile2}
+            />
+            </UserCard>
+            <UserCard
+            headername='Jim and Pam Halpert'>
+            <SingleComment
+            name='Jim Pam'
+            time='Today at 7:00PM'
+            text='Absolutely I do'
+            picture={profile3}
+            />
+            </UserCard>
+            <UserCard
+            headername='Andrew Banies Bernard'>
+            <SingleComment
+            name='Andy'
+            time='Today at 8:00PM'
+            text='I havent proposed to anyone in years'
+            picture={profile4}
+            />
+            </UserCard>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
